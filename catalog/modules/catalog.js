40efb083d98b9c09d1a1ca29210293e06bb71294
xaster-Kies <samueledward40@gmail.com>
2022-04-07 18:55:43 +0100

    db connect and methods

--- catalog/modules/catalog.js
+++ catalog/modules/catalog.js
@@ -1,5 +1,5 @@
 var fs = require('fs')
-var mongoose = require('mongoose')
+var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
 var itemSchema = new Schema ({
@@ -11,6 +11,45 @@ var itemSchema = new Schema ({
 })
 
 var CatalogItem = mongoose.model('Item', itemSchema)
+
+mongoose.connect('mongodb://localhost/catalog')
+var db = mongoose.connection;
+
+db.on('error', console.error.bind(console, 'connection error:'))
+db.once('open', function() {
+  var watch = new CatalogItem({
+    itemName: 9,
+    itemName: "Sports Watch1",
+    brand: 'A1',
+    price: 100,
+    currency: "EUR",
+    categories: ["Watches", "Sports Watches"]
+  })
+})
+
+watch.save((error, item, affectNo) => {
+  if (!error) {
+    console.log('Item added successfully to the catalog');
+  } else {
+    console.log('Cannot add item to the catalog')
+  }
+})
+
+db.once('open', function() {
+  var filter = {
+    'itemName' : 'Sports Watch',
+    'price': 100
+  }
+
+  CatalogItem.find(filter, (error, result) => {
+    if(error) {
+      console.log('Error Occured')
+    } else {
+      console.log('Results found:' + result.length)
+      console.log(result)
+    }
+  })
+})
 
 function readCatalogSync() {
   var file = '../data/catalog.json'
