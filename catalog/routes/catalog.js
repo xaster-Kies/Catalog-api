dabf5b1c758acc4832440e9246327b98ce151f95
xaster-Kies <samueledward40@gmail.com>
2022-04-07 17:35:16 +0100

    GET Routes completed

--- catalog/routes/catalog.js
+++ catalog/routes/catalog.js
@@ -1,9 +1,34 @@
+const { response } = require('express');
 var express = require('express');
+var catalog = require('../modules/catalog.js')
+
 var router = express.Router();
 
-/* GET catalog listing. */
+
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  var categories = catalog.findCategories();
+  response.json(categories)
 });
+
+router.get('/:categoryId', function(req, res, next) {
+  var categories = catalog.findItems(req.params.categoryId)
+  if(categories === undefined) {
+    response.writeHead(404, {'Content-Type' : 'text/plain'})
+    response.end('Not Found')
+  } else {
+    response.json(categories)
+  }
+})
+
+router.get('/:categoryId/:itemId', function(req, res, next) {
+  var item = catalog.findItem(req.params.categoryId, req.params.itemId)
+  if (item === undefined) {
+    response.writeHead(404, {'Content-Type' : 'text/plain'})
+    response.end('Not found')
+  } 
+  else {
+    response.json(item)
+  }
+})
 
 module.exports = router;
