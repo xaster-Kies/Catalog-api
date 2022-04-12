fcb0c6423f47b088ffc912dd52b86754a5d02788
xaster-Kies <samueledward40@gmail.com>
2022-04-12 10:32:12 +0100

    test methods

---
+++ catalog/test/model-test.js
@@ -0,0 +1,43 @@
+var mongoose = require('mongoose')
+var should = require('should')
+var prepare = require('./prepare')
+
+
+const model = require('../model/item')
+const CatalogItem = model.CatalogItem
+
+mongoose.createConnection('mongodb://localhost/catalog');
+
+describe('CatalogItem: models', function () {
+  describe('#create()', function () {
+    it('Should create a new Catalog Item', function (done) {
+
+      var item = {
+        "itemId": "1",
+        "itemName": "1",
+        "price": 100,
+        "currency": "EUR",
+        "categories": [
+          "Watches",
+          "Sports Watches"
+        ]
+      }
+
+      CatalogItem.create(item, function (err, createdItem) {
+        // Check that no error occured
+        should.not.exist(err)
+
+        // Assert that the returned item has is what we expect
+        createdItem.itemId.should.equal('1')
+        createdItem.itemName.should.equal('Sports Watch')
+        createdItem.price.should.equal(100)
+        createdItem.currency.should.equal('EUR')
+        createdItem.categories[0].should.equal('Watches')
+        createdItem.categories[1].should.equal('Sports Watches')
+
+        // Notify mocha that the test has completed
+        done();
+      })
+    })
+  })
+})
--- catalog/tests/model-test.js
+++
@@ -1,6 +0,0 @@
-var mongoose = require('mongoose')
-var should = require('should')
-var prepare = require('./prepare')
-
-
-const model = require('../model')
