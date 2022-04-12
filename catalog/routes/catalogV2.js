bdf10fab5ead61b64b7eb8d8817b7302b65015b7
xaster-Kies <samueledward40@gmail.com>
2022-04-12 19:05:10 +0100

    v2 populate

--- catalog/routes/catalogV2.js
+++ catalog/routes/catalogV2.js
@@ -1,14 +1,14 @@
 var express = require('express');
 var router = express.Router();
 
-var catalog = require('../modules/catalogV1.js')
+var catalogV2 = require('../modules/catalogV2.js')
 
-router.get('/v1/', function(req, res, next) {
+router.get('/v2/', function(req, res, next) {
   var categories = catalog.findCategories();
   res.json(categories)
 });
 
-router.get('/:categoryId', function(req, res, next) {
+router.get('/v2/:categoryId', function(req, res, next) {
   var categories = catalog.findItems(req.params.categoryId)
   if(categories === undefined) {
     res.writeHead(404, {'Content-Type' : 'text/plain'})
@@ -18,7 +18,7 @@ router.get('/:categoryId', function(req, res, next) {
   }
 })
 
-router.get('/:categoryId/:itemId', function(req, res, next) {
+router.get('/v2/:categoryId/:itemId', function(req, res, next) {
   var item = catalog.findItem(req.params.categoryId, req.params.itemId)
   if (item === undefined) {
     res.writeHead(404, {'Content-Type' : 'text/plain'})
@@ -26,6 +26,17 @@ router.get('/:categoryId/:itemId', function(req, res, next) {
   } 
   else {
     res.json(item)
+  }
+})
+
+router.get('/v2/items', function (request, response) {
+  var getParams = url.parse(request.url, true).query;
+  if(Object.keys(getParams).length == 0) {
+    catalogV2.findAllItems(response)
+  } else {
+    var key = Object.keys(getParams)[0]
+    var value = getParams[key]
+    catalogV2.findItemsByAttribute(key, value, response)
   }
 })
 
