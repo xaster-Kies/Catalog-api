680959d5afc8e7a237518e4d71435e56c1be3aa7
xaster-Kies <samueledward40@gmail.com>
2022-04-15 09:07:16 +0100

    passport auth

--- catalog/routes/catalogV1.js
+++ catalog/routes/catalogV1.js
@@ -1,9 +1,18 @@
 var express = require('express');
+const passport = require('passport')
+const BasicStrategy = require('passport-http').BasicStrategy
 var router = express.Router();
+
+passport.use(new BasicStrategy(function(username, password, done) {
+  if(username == 'user' && password=='default') {
+    return done(null, username)
+  }
+}))
 
 var catalog = require('../modules/catalogV1.js')
 
 router.get('/v1/', function(req, res, next) {
+  passport.authenticate(basic, { session: false })
   var categories = catalog.findCategories();
   res.json(categories)
 });
